524ff4aec8ffdb51631da839df555f585b284377
AugustoRickes <augusto.rickes@atlastechnol.com>
2024-11-15 23:00:29 -0300

    feat: criacao das regras na model de nf

--- app/Models/NotaFiscal.php
+++ app/Models/NotaFiscal.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class NotaFiscal extends Model
 {
     use HasFactory;
+
+    protected $table = 'notafiscal';
+
+    protected $primaryKey = 'nota_fiscal_id';
+
+    public $incrementing = false;
+
+    protected $fillable = [
+        'nota_fiscal_id',
+        'usuario_uuid',
+        'estabelecimento_cnpj',
+        'data_emissao',
+        'valor_total'
+    ];
+
+    public function itens()
+    {
+        return $this->hasMany(ItemNotaFiscal::class, 'nota_fiscal_id', 'nota_fiscal_id');
+    }
+
+    public function usuario()
+    {
+        return $this->belongsTo(User::class, 'usuario_uuid', 'id');
+    }
 }

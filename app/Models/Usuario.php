8a2694f602223682821b36fa42b617737b720ffd
AugustoRickes <augusto.rickes@atlastechnol.com>
2024-11-17 09:39:23 -0300

    fix: comentei o usuario para nao confundir com user

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -1,33 +1,33 @@
 <?php
 
-namespace App\Models;
+//namespace App\Models;
+//
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Model;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Usuario extends Model
-{
-    use HasFactory;
-
-    protected $table = 'usuario';
-
-    protected $primaryKey = 'uuid';
-    public $incrementing = false;
-    protected $keyType = 'string';
-
-    protected $fillable = [
-//        'uuid',
-//        'nome',
-        'cidade',
-        'estado',
-        'localizacao',
-        'tem_carro',
-        'km_por_litro',
-        'kudos',
-    ];
-
-    public function notasFiscais()
-    {
-        return $this->hasMany(NotaFiscal::class, 'usuario_uuid', 'uuid');
-    }
-}
+//class Usuario extends Model
+//{
+//    use HasFactory;
+//
+//    protected $table = 'usuario';
+//
+//    protected $primaryKey = 'uuid';
+//    public $incrementing = false;
+//    protected $keyType = 'string';
+//
+//    protected $fillable = [
+////        'uuid',
+////        'nome',
+//        'cidade',
+//        'estado',
+//        'localizacao',
+//        'tem_carro',
+//        'km_por_litro',
+//        'kudos',
+//    ];
+//
+//    public function notasFiscais()
+//    {
+//        return $this->hasMany(NotaFiscal::class, 'usuario_uuid', 'uuid');
+//    }
+//}
